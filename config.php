31fdcf7982568fd274e7f461ac4a7ddb850c37a6
Dason Baird <dbaird2@csub.edu>
2025-05-24 23:22:28 -0700

    waef

--- config.php
+++ config.php
@@ -1,28 +1,29 @@
 <?php
 use Dotenv\Dotenv;
 require __DIR__ . '/vendor/autoload.php';
-
-
-
 if (file_exists(__DIR__ . '/.env')) {
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
-
 $db_host = $_ENV['DB_HOST'] ?? NULL;
 $db_port = $_ENV['DB_PORT'] ?? NULL;
 $db_name = $_ENV['DB_NAME'] ?? NULL;
 $db_user = $_ENV['DB_USER'] ?? NULL;
 $db_pass = $_ENV['DB_PASS'] ?? NULL;
 
+
 try {
     $dbh = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass, array());
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    header("location: /ConnectionFailed");
     error_log($e->getMessage());
     die('Connection Failed');
 } 
+echo "<h3>$db_host </h3>";
+echo "<h3>$db_port </h3>";
+echo "<h3>$db_name </h3>";
+echo "<h3>$db_user </h3>";
+echo "<h3>$db_pass </h3>";
 ?>
 
 
