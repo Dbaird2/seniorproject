51396457334d88c8630212fcd420b1bae5174d8e
Dason Baird <dbaird2@csub.edu>
2025-06-10 20:50:07 -0700

    Ajax.php

--- search/Ajax.php
+++ search/Ajax.php
@@ -74,7 +74,7 @@ error_reporting(0);
 <?php
 if (isset($_POST['search']) || isset($_GET['search'])) {
     $tag = $_POST['search'];
-    $offset = isset($_POST['offset']) ? $_POST['offset'] : 1;
+    $offset = isset($_POST['offset']) ? (int)$_POST['offset'] : 1;
     $result = [];
     if (isset($_SESSION['role'])) {
         $query = "SELECT a.asset_tag, a.asset_name, a.serial_num, a.asset_price, a.po, a.room_tag, a.dept_id FROM asset_info as a WHERE asset_tag LIKE :tag OR asset_name LIKE :tag OR serial_num LIKE :tag OR CAST(po as CHAR) LIKE :tag OR dept_id LIKE :tag LIMIT 50 OFFSET :offset";
