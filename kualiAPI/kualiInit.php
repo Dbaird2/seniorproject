6428d0c64de220f9d39b3c5788adc8beae8c6ba2
Caroline Contreras <ccontreras2@odin.cs.csubak.edu>
2025-06-24 12:10:03 -0700

    this is my attempt at moving it into a folder

---
+++ kualiAPI/kualiInit.php
@@ -0,0 +1,45 @@
+<?php
+
+$subdomain = "subdomain";
+$apikey = "apikey";
+
+$url = "";
+
+//initialize a cURL session
+$curl = curl_init($url);
+//Sets URL to fetch
+curl_setopt($curl, CURLOPT_URL, $url);
+
+curl_setopt($curl, CURLOPT_POST, true);
+//Return the transfer as a string
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+	"Content-Type: application/json",
+	"Authorization: Bearer {$apikey}",
+);
+
+// ...(cURL handle returned by curl_init(), Option you want to set, value you want to set)
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+
+$data = '{"query": "mutation" ($appId: ID!) { InitializeWorkflow(args: {id:
+$appId}) {actionId}}", "variables": {
+	"appId": ""
+}}';
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//debug
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+//execute a cURL session
+$resp = curl_exec($curl);
+
+//Close the cURL session
+curl_close($curl);
+var_dump($resp);
+
+
