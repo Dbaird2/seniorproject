0612e410444e21c39fa3067373b1fc9bbf92c22c
Caroline Contreras <ccontreras2@odin.cs.csubak.edu>
2025-06-24 09:25:37 -0700

    My first commit, just some notes on Kuali

---
+++ kualiInit.php
@@ -0,0 +1,23 @@
+<?php
+
+$subdomain = "subdomain";
+$apikey = "apikey";
+
+$url = "";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+	"Content-Type: application/json",
+	"Authorization: Bearer {$apikey}",
+);
+
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = '{"query": "mutation" ($appId: ID!) { InitializeWorkflow(args: {id:
+$appId}) {actionId}}", "variables": {
+	"appId": ""
+}};
