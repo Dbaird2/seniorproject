7d43502acc326ca5fa7687ab95366a75f6e5a080
Dason Baird <dbaird2@csub.edu>
2025-07-01 10:34:37 -0700

    change download.php

--- audit/download.php
+++ audit/download.php
@@ -1,20 +1,20 @@
 <?php
 error_reporting(0);
-require __DIR__ . '/vendor/autoload.php';
 
 if (isset($_GET['file'])) {
-    $baseDir = __DIR__ . '/exports/';
+    //$baseDir = __DIR__ . '/exports/';
     $fileName = basename(urldecode($_GET['file']));
-    $filePath = $baseDir . $fileName;
+    $filePath = /*$baseDir .*/ $fileName;
 
     if (file_exists($filePath)) {
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment; filename="' . $fileName . '"');
+        header('Content-Disposition: attachment; filename="' . basename($filePath) . '"');
         header('Cache-Control: max-age=0');
-        header('Content-Transfer-Encoding: binary');
-        header('Content-Length: ' . filesize($filePath));
-        flush();
+        //header('Content-Transfer-Encoding: binary');
+        //header('Content-Length: ' . filesize($filePath));
+        //flush();
         readfile($filePath);
+        unlink($filePath);
         exit;
     } else {
         echo "File does not exist.";
