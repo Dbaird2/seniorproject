68633f4ed290b2429d0a6bdf8711981f27cbf91a
Dason Baird <dbaird2@csub.edu>
2025-07-03 16:35:08 -0700

    Ajax styling

--- search/Ajax.php
+++ search/Ajax.php
@@ -43,13 +43,10 @@ error_reporting(E_ALL);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <style>
-     body {
+body {
             margin: 0;
             height: 100vh;
             font-size: calc(0.5vw + 0.4vh);
-            width: 100%;
-            position: absolute;
-            top: 8vh;
         }
         body::-webkit-scrollbar {
             width: 1em;
@@ -104,18 +101,21 @@ error_reporting(E_ALL);
         .row-odd {
             background-color: #ffffff;
         }
-.asset-table {
-            width: 100%;
+        .asset-table {
+            font-size: calc(0.7vw + 0.4vh);
+
             margin: auto; border: 1px solid #c0d6e4;
             border-collapse: collapse;
         }
         th {
+            font-size: calc(0.7vw + 0.4vh);
             color:#2c3e50;
             padding: 10px;
             border: 1px solid #ddd;
             text-align: left;
         }
         td {
+            font-size: calc(0.6vw + 0.4vh);
             font-weight: 500;
             color:#2c3e50;
             padding: 10px;
@@ -126,6 +126,40 @@ error_reporting(E_ALL);
         .table-div {
             display:flex;
             justify-content: center;
+        }
+        #button-9 {
+            appearance: button;
+            backface-visibility: hidden;
+            background-color: #405cf5;
+            border-radius: 6px;
+            border-width: 0;
+            box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset,rgba(50, 50, 93, .1) 0 2px 5px 0,rgba(0, 0, 0, .07) 0 1px 1px 0;
+            box-sizing: border-box;
+            color: #fff;
+            cursor: pointer;
+            font-family: -apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif;
+            font-size: calc(0.6vw + 0.4vh);
+            height: 44px;
+            line-height: 1.15;
+            margin: 12px 0 0;
+            outline: none;
+            overflow: hidden;
+            padding: 0 25px;
+            position: relative;
+            text-align: center;
+            text-transform: none;
+            transform: translateZ(0);
+            transition: all .2s,box-shadow .08s ease-in;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+        }
+
+        #button-9:disabled {
+            cursor: default;
+        }
+        #button-9:focus {
+            box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .2) 0 6px 15px 0, rgba(0, 0, 0, .1) 0 2px 2px 0, rgba(50, 151, 211, .3) 0 0 0 4px;
         }
         </style>
 <?php
@@ -315,7 +349,7 @@ if (isset($_POST['search']) || isset($_GET['search'])) {
             ?>
             <td class=<?=$color_class?>><?=$row_num++?></td>
             <td class=<?=$color_class?>>
-                    <button  data-toggle="modal" data-target="#modal<?= $safe_tag?>"><?= $safe_tag?></button>
+                    <button id="button-9"  data-toggle="modal" data-target="#modal<?= $safe_tag?>"><?= $safe_tag?></button>
                 </td>
                 <?php if(array_key_exists('asset_name', $header_true)) {echo "<td class=".$color_class.">".$safe_name."</td>";}?>
                 <?php if(array_key_exists('dept_id', $header_true)) {echo "<td class=".$color_class.">".$safe_deptid."</td>";}?>
