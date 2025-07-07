b3c44eab4cb60c8d15add132f8cb2bb508a98bbd
Dason Baird <dbaird2@csub.edu>
2025-07-07 15:05:37 -0700

    got rid of warnings

--- audit/auditing.php
+++ audit/auditing.php
@@ -1,11 +1,11 @@
 <?php
 error_reporting(0);
 include_once("../config.php");
-
+/*
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
-
+ */
 $filePath = $count = NULL;
 $previous_inputs = [];
 
