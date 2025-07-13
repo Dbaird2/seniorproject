86037d8b46d663dcd27ab4c0886120939fa4ab11
Dason Baird <dbaird2@csub.edu>
2025-07-13 13:00:11 -0700

    added responsiveness & validation

--- add-assets/add-asset.php
+++ add-assets/add-asset.php
@@ -51,21 +51,26 @@ foreach($result as $row) {
     <div class="space-y-1">
       <label for="tag" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Asset Tag</label>
       <input id="tag" type="text" placeholder="Ex: 12345" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+      <p id="p-tag" class=" hidden mt-3 text-sm/6 text-red-400">Asset Tag cannot be empty</p>
     </div>
 
     <div class="space-y-1">
       <label for="descr" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Asset Description</label>
       <input id="descr" type="text" placeholder="Ex: DELL LATITUDE 5450" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+      <p id="p-descr" class=" hidden mt-3 text-sm/6 text-red-400">Asset Description cannot be empty</p>
     </div>
 
     <div class="space-y-1">
       <label for="type" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Asset Type</label>
         <input id="type" aria-expanded="false" placeholder="Ex: Equipment" type="text" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+
     </div>
 
     <div class="space-y-1">
       <label for="acq-date" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Acquisition Date</label>
       <input id="acq-date" type="date" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer appearance-none" />
+      <p id="p-acq-date" class=" hidden mt-3 text-sm/6 text-red-400">Acquisition Date cannot be empty</p>
+      <p id="t-acq-date" class=" hidden mt-3 text-sm/6 text-red-400">Acquisition Date cannot be in the future</p>
     </div>
 
     <div class="space-y-1">
@@ -80,7 +85,8 @@ foreach($result as $row) {
 
     <div class="space-y-1">
       <label for="cost" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Total Cost</label>
-        <input id="cost" aria-expanded="false" placeholder="Ex: 1060.50" type="text" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+        <input id="cost" aria-expanded="false" placeholder="Ex: 1060.50" type="number" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+        <p id="p-cost" class=" hidden mt-3 text-sm/6 text-red-400">Total Cost cannot be empty</p>
     </div>
 
     <div class="space-y-1">
@@ -90,6 +96,8 @@ foreach($result as $row) {
                     <option  class="option block px-4 py-2 text-sm text-blue-800 hover:bg-blue-100 rounded-md"><?=$row['dept_id']?></option>
            <?php } ?>
         </select>
+        <p id="p-dept-id" class=" hidden mt-3 text-sm/6 text-red-400">Department ID cannot be empty</p>
+
         </div>
 
         <div class="space-y-1">
@@ -99,13 +107,15 @@ foreach($result as $row) {
                         <option  class="option block px-4 py-2 text-sm text-blue-800 hover:bg-blue-100 rounded-md"><?=$row?></option>
                <?php } ?>
             </select>
+              <p id="p-bldg-name" class=" hidden mt-3 text-sm/6 text-red-400">Building Name cannot be empty</p>
             </div>
 
     <div class="space-y-1">
       <label for="room-names" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Room Name/Number</label>
 <select data-dui-toggle="dropdown" aria-expanded="false" id="room-names" placeholder="Select Gender" type="text"  class="w-full mt-1 aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer">
-
         </select>
+        <p id="p-room-names" class=" hidden mt-3 text-sm/6 text-red-400">Room Name cannot be empty</p>
+
         </div>
 
 
@@ -119,7 +129,9 @@ foreach($result as $row) {
 </div>
 <div class="space-y-1">
       <label for="po" class="block mb-1 text-sm font-semibold antialiased text-blue-800">Purchase Order</label>
-        <input id="po" aria-expanded="false" placeholder="Ex: 1001023403" type="text" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+      <input id="po" aria-expanded="false" placeholder="Ex: 1001023403" type="number" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer" />
+      <p id="p-po" class=" hidden mt-3 text-sm/6 text-red-400">Purchase Order cannot be empty</p>
+
     </div>
 
     <div class="space-y-1">
@@ -127,13 +139,13 @@ foreach($result as $row) {
             <textarea name="about" id="notes" rows="3" class="w-full aria-disabled:cursor-not-allowed outline-none focus:outline-none text-blue-800 dark:text-black placeholder:text-blue-600/60 ring-transparent border border-blue-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-blue-300 hover:ring-none focus:border-blue-400 focus:ring-none peer"></textarea>
             <p class="mt-3 text-sm/6 text-gray-600">Extra notes for the asset.</p>
     </div>
-
-
-
   </div>
   <div class="item flex flex-wrap justify-center gap-4">
     <button id="submit" onclick="submitPage()" class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed focus:shadow-none text-sm py-2 px-4 shadow-sm hover:shadow-md bg-stone-800 hover:bg-blue-900 relative bg-opacity-0 border-blue-500 text-black rounded-lg hover:border-blue-900 after:absolute after:inset-0 after:rounded-[inherit] after:box-shadow after:shadow-[inset_0_1px_0px_rgba(255,255,255,0.25),inset_0_-2px_0px_rgba(0,0,0,0.35)] after:pointer-events-none transition-all antialiased">Add Asset</button>
     </div>
+    <p id="success" class=" hidden mt-3 flex justify-center text-base/6 text-green-500">Successfully added asset</p>
+    <p id="fail" class=" hidden mt-3 flex justify-center text-base/6 text-red-400">Failed to add asset</p>
+
 </div>
 
 
@@ -144,6 +156,17 @@ For mass/bulk asset adding <a href="https://www.7b7x-dataworks.onrender.com/add-
 <script>
 
 result = <?= json_encode($result)?>;
+
+function checkEmpty(variable, id) {
+    const ele = document.getElementById(id);
+    if (variable == "" || variable < 0) {
+        ele.classList.remove("hidden"); 
+        return false;
+    } else {
+        ele.classList.add("hidden");
+        return true;
+    }
+}
 
 function submitPage() {
     const profile = document.getElementById('profile').value;
@@ -158,36 +181,61 @@ function submitPage() {
     const acq_date = document.getElementById('acq-date').value;
     const type = document.getElementById('type').value;
     const notes = document.getElementById('notes').value;
-
+    const cost = document.getElementById('cost').value;
     const url = "insert-asset.php";
 
-    try {
-        fetch(url, {
-        method: "POST",
-            body: JSON.stringify ({
-            tag: tag,
-                descr: descr,
-                sn: sn,
-                po: po,
-                model: model,
-                acq_date: acq_date,
-                profile: profile,
-                type: type,
-                dept_id: dept_id,
-                room_name: room_name,
-                bldg_name: bldg_name,
-                notes: notes
-    }),
-        headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-    })
-        .then((response) =>response.json)
-        .then((json) =>console.log(json));
-    } catch (error) {
-        console.log(error);
-    }
+    room_c = checkEmpty(room_name, "p-room-names");  
+    bldg_c = checkEmpty(bldg_name, "p-bldg-name");  
+    tag_c = checkEmpty(tag, "p-tag");      
+    date_c = checkEmpty(acq_date, "p-acq-date");    
+    dept_c = checkEmpty(dept_id, "p-dept-id");
+    po_c = checkEmpty(po, "p-po");
+    cost_c = checkEmpty(cost, "p-cost");
+    descr_c = checkEmpty(descr, "p-descr");
 
+    now_date = Date.now("YYYY-MM-DD");
+    input_date = Date.parse(acq_date);
+
+    time_diff = now_date - input_date;
+    time_good = true;
+    date_t = checkEmpty(time_diff, "t-acq-date");    
+
+    if (room_c && bldg_c && tag_c && date_c && dept_c && po_c && cost_c && descr_c && date_t) {
+        try {
+            fetch(url, {
+            method: "POST",
+                body: JSON.stringify ({
+                tag: tag,
+                    descr: descr,
+                    sn: sn,
+                    po: po,
+                    model: model,
+                    acq_date: acq_date,
+                    profile: profile,
+                    type: type,
+                    dept_id: dept_id,
+                    room_name: room_name,
+                    bldg_name: bldg_name,
+                    notes: notes
+        }),
+          headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+            .then((response) =>response.json())
+            .then((json) => {const success = document.getElementById('success');const fail = document.getElementById("fail");
+            if (json['status'] === 'success') {
+                success.classList.remove('hidden');
+                fail.classList.add("hidden");
+            } else {
+                success.classList.add('hidden'); 
+                fail.classList.remove("hidden")
+            }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => { 
