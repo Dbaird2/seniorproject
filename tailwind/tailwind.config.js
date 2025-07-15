d23dfae49681c391cbb5dc4afc2536dd129e13bb
Dason Baird <dbaird2@csub.edu>
2025-07-15 08:06:56 -0700

    changed path in tailwind.config

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ["./**/*.php","./navbar.php", "./**/*.html", "./**/*.js"],
+    content: ["../**/*.php","../navbar.php", "../**/*.html", "../**/*.js"],
     theme: {
         extend: {
             animation: {
