e8b67ad6e3eb594d97d74a2a322a8b0b170e8933
Dason Baird <dbaird2@csub.edu>
2025-07-15 16:05:13 -0700

    changed navbar.php look

--- navbar.php
+++ navbar.php
@@ -1,58 +1,121 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navbar</title>
+  <link rel="stylesheet" href="/navbar.css">
+</head>
+<?php $_SESSION['role'] = 'Management'; ?>
+<body>
+  <header>
+    <?php if(isset($_SESSION['role']) && $_SESSION['role'] !== '' && $_SESSION['role'] !== NULL) { ?>
+    <span class="dropdown">
 
-<style>
-body,h1,h2,h3,h4,h5,h6 {    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;}
-.w3-bar,h1,button {    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;}
-.fa-anchor,.fa-coffee {font-size:200px}
+    <a class="dropbtn" href="#"><span>
+<svg width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+     stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+  <line x1="3" y1="6" x2="21" y2="6"/>
+  <line x1="3" y1="12" x2="21" y2="12"/>
+  <line x1="3" y1="18" x2="21" y2="18"/>
+</svg>
+<div class="dropdown-content">
+  <a><img src="https://th.bing.com/th/id/OIP.jwU-GwZPqzDTyOxeKaZ2XgHaEz?w=247&h=180&c=7&r=0&o=7&pid=1.7&rm=3" alt="CSUB Roadrunner" height="130" width="180">
+</a>
 
-</style>
-
-<!-- Navbar -->
-<div class="screen-size w3-animate-opacity w3-top" >
-  <div class="w3-bar w3-cobalt w3-card w3-small">
-    <a class="w3-bar-item w3-button w3-hide-small w3-hide-small w3-right w3-padding-small w3-hover-white w3-small w3-cobalt" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="https://dataworks-7b7x.onrender.com/index.php" class="w3-bar-item w3-button w3-hover-white w3-padding-small w3-left-align">Home</a>
-<?php
-if (isset($_SESSION['id'])) {
-?>
-    <a href="https://dataworks-7b7x.onrender.com/audit/upload.php" class="w3-bar-item w3-button w3-hide-small w3-padding-small w3-hover-white  w3-left-align">Start an Audit</a>
-    <a href="https://dataworks-7b7x.onrender.com/search/search.php" class="w3-bar-item w3-button w3-hide-small w3-padding-small w3-hover-white  w3-left-align">Search Assets</a>
-    <a href="https://dataworks-7b7x.onrender.com/auth/logout.php" class="w3-bar-item w3-button w3-padding-small w3-white w3-right-align w3-right">Logout</a>
-<?php
-} else {
-?>
-    <a href="https://dataworks-7b7x.onrender.com/auth/login.php" class="w3-bar-item w3-button w3-padding-small w3-white w3-right-align w3-right">Login</a>
-<?php
-}
-if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
-?>
-    <a href="https://dataworks-7b7x.onrender.com/auth/signup.php" class="w3-bar-item w3-button w3-padding-small w3-right-align w3-right">Signup</a>
-    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-dept.php" class="w3-bar-item w3-button w3-padding-small w3-right-align w3-right">Add Departments</a>
-    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-bldg.php" class="w3-bar-item w3-button w3-padding-small w3-right-align w3-right">Add Building/Rooms</a>
-<?php
-  }
-?>
-   </div>
-  <div id="navDemo" class="w3-sidebar w3-hover-cobalt w3-light-gray w3-hide w3-hide-small w3-bar-block w3-hide-small w3-small w3-animate-right" style="height:7vh;width:6vw;right:0">
-    <a href="https://dataworks-7b7x.onrender.com/audit/auditing.php" class="w3-bar-item w3-button w3-padding-small">Start an Audit</a>
-    <a href="https://dataworks-7b7x.onrender.com/help.php" class="w3-bar-item w3-button w3-padding-small">Help</a>
-  </div>
+<a href="https://dataworks-7b7x.onrender.com/audit/upload.php">Audit</a>
+<a href="https://dataworks-7b7x.onrender.com/search/search.php">Search</a>
+<?php if ($_SESSION['role'] === 'admin') { ?>
+  <span class="dropdown2">
+  <a  href="#">Add<svg width="13" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg></a>
+<span class="dropdown-content2">
+    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-asset.php" class="dropdown-element">Asset</a>
+    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-dept.php" class="dropdown-element">Department</a>
+    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-bldg.php" class="dropdown-element">Building/Rooms</a>
+</span>
+</span>
+<?php } ?>
+<h4 class="">Management</h4>
+<a href="https://dataworks-7b7x.onrender.com/auth/settings.php">Settings</a>
+<a href="https://dataworks-7b7x.onrender.com/auth/logout.php">Logout</a>
+<?php if ($_SESSION['role'] === 'admin') { ?>
+<span class="dropdown2">
+  <a class="dropbtn2" href="#">Admin<svg width="13" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg></a>
+  <span class="dropdown-content2">
+    <a href="https://dataworks-7b7x.onrender.com/admin/edit-user.php" class="dropdown-element">Edit User</a>
+    <a href="https://dataworks-7b7x.onrender.com/auth/signup.php" class="dropdown-element">Signup User</a>
+  </span>
+</span>
+<?php } ?>
 </div>
+<?php } ?>
+    </span></a></span>
+    <h2 class="gradient-text">CSUB.</h2>
+        <?php if(isset($_SESSION['role']) && $_SESSION['role'] !== '' && $_SESSION['role'] !== NULL) { ?>
 
+    <nav id="nav_links">
+      <ul  class="nav_links">
+        
+        <li><a href="https://dataworks-7b7x.onrender.com/audit/upload.php">Audit</a></li>
+        <li><a href="https://dataworks-7b7x.onrender.com/search/search.php">Search</a></li>
 
+<?php if ($_SESSION['role'] === 'admin') { ?>
+        <li>
+<span class="dropdown">
+  <a class="dropbtn">Add<svg width="13" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</a>
+  <span class="dropdown-content">
+    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-asset.php" class="dropdown-element">Asset</a>
+    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-dpet.php" class="dropdown-element">Department</a>
+    <a href="https://dataworks-7b7x.onrender.com/add-assets/add-bldg.php" class="dropdown-element">Building/Rooms</a>
+  </span>
+</span></li>
+
+<li> 
+  <span class="dropdown">
+    <a class="dropbtn">Admin<svg width="13" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </a>
+  <span class="dropdown-content">
+    <a href="https://dataworks-7b7x.onrender.com/admin/edit-user.php" class="dropdown-element">Edit User</a>
+    <a href="https://dataworks-7b7x.onrender.com/auth/signup.php"class="dropdown-element">Signup User</a>
+  </span>
+</span></li>
+<?php } ?>
+</ul>
+    </nav>
+    <a class="cta" href="https://dataworks-7b7x.onrender.com/auth/Logout.php"><button>Logout</button></a>
+    <?php } else { ?>
+    <a class="cta" href="https://dataworks-7b7x.onrender.com/auth/Login.php"><button>Login</button></a>
+    <?php } ?>
+
+  </header>
+</body>
+</html>
 <script>
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
+ 
+  document.addEventListener("DOMContentLoaded", function () {
+    const width = window.screen.width;
+    const height = window.screen.height;
+    window.addEventListener("resize", () => {
+    const width = window.screen.width;
+    const height = window.screen.height;
+    const nav_links = document.getElementById("nav_links");
 
+    
+    if (width <= 778) {
+      nav_links.hidden = true;
+    } else {
+      nav_links.hidden = false;
+    }
+  });
+
+});
 </script>
