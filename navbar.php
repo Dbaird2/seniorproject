e79171c6acf1c64c392ddff48a00287daad46143
Dason Baird <dbaird2@csub.edu>
2025-07-24 12:55:48 -0700

    style changes for better shifting time

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dataworks</title>
-  <link rel="stylesheet" href="/navbar.css">
+  <link rel="preload" href="/navbar.css" as="style">
 </head>
 <body>
     <div class="has-navbar">
