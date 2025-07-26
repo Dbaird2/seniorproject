7adee5ec65b1b665c5118e206ef171f4da57ebf4
Dason Baird <dbaird2@csub.edu>
2025-07-25 19:22:59 -0700

    typo

--- search/tables_layout.php
+++ search/tables_layout.php
@@ -4,7 +4,7 @@
 function asset_layout($result, $header_true, $row_num)
 {
 ?>
-    <section class="is-ajax" style="display:none;">
+    <section class="is-ajax">
         <div class="table-div">
             <table id="asset-table">
                 <thead>
@@ -458,10 +458,3 @@ function user_layout($result, $row_num)
 <?php
 }
 ?>
-<script>
-window.addEventListener("load", function() {
-   //document.getElementbyId("loader").style.display="none";
-    const show_result = document.querySelector(".is-ajax");
-    show_result.style.display = "block";
-);
-</script>
