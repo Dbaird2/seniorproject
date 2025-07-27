4beb6dae4b07580521a8aa5b3ca717645c833cdf
Dason Baird <dbaird2@csub.edu>
2025-07-27 12:48:59 -0700

    should work with audit from db

--- audit/audit-history/audit-details.php
+++ audit/audit-history/audit-details.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once(__DIR__ . "/../../config.php");
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -39,40 +39,94 @@ try {
         $keys = false;
         $count = count($audit_data[1]);
         $total_count = count($audit_data);
-        
+        $header_copy = array_unique($header_copy);
+        $counter = count($audit_data_copy['Tag Number']);
+        for ($i = 0; $i < $counter; $i++) {
+            foreach ($found_data as $found_row) {
+                if ($audit_data_copy['Tag Number'][$i] == $found_row['Asset Tag']) {
+                    $audit_data_copy['Audit Notes'][$i] = $found_row['Asset Note'];
+                    $audit_data_copy['Audit Date'][$i] = date('Y-m-d H:i:s', strtotime((string) $found_row['Time Scanned']));
+                    $audit_data_copy['Room Tag'][$i] = $found_row['Found Room'];
+                }
+            }
+        }
+        $j = 0;
+        foreach ($found_data as $found_row) {
+            if (!in_array($found_row['Asset Tag'], $audit_data_copy['Tag Number'])) {
+                $audit_data_copy['Tag Number'][($total_count+$j)] = $found_row['Asset Tag'];
+                $audit_data_copy['Audit Notes'][($total_count+$j)] = $found_row['Asset Note'] ?? 'No Notes';
+                $audit_data_copy['Audit Date'][($total_count+$j)] = date('Y-m-d H:i:s', strtotime((string) $found_row['Time Scanned']));
+                $audit_data_copy['Room Tag'][($total_count+$j)] = $found_row['Found Room'];
+                $j++;
+            }
+        }
+        $header_copy[] = 'Audit Notes';
+        $header_copy[] = 'Audit Date';
+        $header_copy[] = 'Room Tag';
+
     } else {
         $audit_headers = array_keys($audit_data[0]);
         $count = count($audit_data[0]);
         $keys = true;
+=        foreach ($audit_headers as $header_index => $header) {
+            if ($header === 'po') {
+                $headers_copy[$header_index] = 'Purchase Order';
+            } elseif ($header === 'dept_id') {
+                $headers_copy[$header_index] = 'Department ID';
+            } elseif ($header === 'room_loc') {
+                $headers_copy[$header_index] = 'Room Number';
+            } elseif ($header === 'room_tag') {
+                $headers_copy[$header_index] = 'Room Tag';
+            } elseif ($header === 'asset_tag') {
+                $headers_copy[$header_index] = 'Tag Number';
+            } elseif ($header === 'bldg_name') {
+                $headers_copy[$header_index] = 'Building Name';
+            } elseif ($header === 'asset_name') {
+                $headers_copy[$header_index] = 'Asset Name';
+            } elseif ($header === 'serial_num') {
+                $headers_copy[$header_index] = 'Serial Number';
+            } elseif ($header === 'asset_price') {
+                $headers_copy[$header_index] = 'Cost';
+            }
+        }
+        $audit_headers[] = 'Audit Notes';
+        $audit_headers[] = 'Audit Date';
+        $audit_headers[] = 'Room Tag';
+        $headers_copy[] = 'Audit Notes';
+        $headers_copy[] = 'Audit Date';
+        $headers_copy[] = 'Room Tag';
+        $audit_data_copy = $audit_data;
+        $counter = count($audit_data_copy);
+
+        for ($i = 0; $i < $counter; $i++) {
+            foreach ($found_data as $found_index => $found_row) {
+                if ($audit_data_copy[$i]['asset_tag'] === $found_row['Asset Tag']) {
+                    $audit_data_copy[$i]['Audit Date'] = date('Y-m-d H:i:s', strtotime((string) $found_row['Time Scanned']));
+                    $audit_data_copy[$i]['Room Tag'] = $found_row['Found Room'];
+                    $audit_data_copy[$i]['Audit Notes'] = $found_row['Asset Note'];
+                    $found_data[$found_index]['found'] = true;
+                }
+            }
+        }
+        $j = 0;
+        foreach ($found_data as $found_row) {
+            if (!isset($found_row['found']) || !$found_row['found']) {
+                $audit_data_copy[($counter+$j)]['asset_tag'] = $found_row['Asset Tag'];
+                $audit_data_copy[($counter+$j)]['Audit Notes'] = $found_row['Asset Note'] ?? 'No Notes';
+                $audit_data_copy[($counter+$j)]['Audit Date'] = date('Y-m-d H:i:s', strtotime((string) $found_row['Time Scanned']));
+                $audit_data_copy[($counter+$j)]['Room Tag'] = $found_row['Found Room'];
+                $j++;
+            }
+           
+        }
+        $counter = $counter + $j;
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
 
-$header_copy = array_unique($header_copy);
-$header_copy[] = 'Audit Notes';
-$header_copy[] = 'Audit Date';
-$header_copy[] = 'Room Tag';
-$counter = count($audit_data_copy['Tag Number']);
-for ($i = 0; $i < $counter; $i++) {
-    foreach ($found_data as $found_row) {
-        if ($audit_data_copy['Tag Number'][$i] == $found_row['Asset Tag']) {
-            $audit_data_copy['Audit Notes'][$i] = $found_row['Asset Note'];
-            $audit_data_copy['Audit Date'][$i] = date('Y-m-d H:i:s', strtotime((string) $found_row['Time Scanned']));
-            $audit_data_copy['Room Tag'][$i] = $found_row['Found Room'];
-        }
-    }
-}
-$j = 0;
-foreach ($found_data as $found_row) {
-    if (!in_array($found_row['Asset Tag'], $audit_data_copy['Tag Number'])) {
-        $audit_data_copy['Tag Number'][($total_count+$j)] = $found_row['Asset Tag'];
-        $audit_data_copy['Audit Notes'][($total_count+$j)] = $found_row['Asset Note'] ?? 'No Notes';
-        $audit_data_copy['Audit Date'][($total_count+$j)] = date('Y-m-d H:i:s', strtotime((string) $found_row['Time Scanned']));
-        $audit_data_copy['Room Tag'][($total_count+$j)] = $found_row['Found Room'];
-        $j++;
-    }
-}
+
+
 ob_start(); 
 
     ?>
@@ -94,7 +148,7 @@ ob_start();
             </tr>
         </thead>
         <tbody>
-        <?php
+            <?php
             $i= 0;
             $color = ($i++ % 2 === 0) ? 'even' : 'odd';
             echo "<tr class='$color'>";
@@ -106,6 +160,7 @@ ob_start();
                         ?>
         </tbody>
     </table>
+    <?php if (!$keys) { ?>
     <section class="middle">
     
 <div class="audit-data">
@@ -150,8 +205,49 @@ ob_start();
                             </table>
                                 </div>
                                         </section>
+<?php } else { ?>
+    <h3>Department <?php echo htmlspecialchars((string) $audit_details['dept_id']); ?> Assets</h3>
+    <table>
+        <thead>
+            <tr>
+                <?php foreach ($headers_copy as $header): ?>
+                    <th class='odd'><?php echo htmlspecialchars((string) $header); ?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php  $j = 1;
+            for ($i = 0; $i < $counter; $i++): 
+                $color = ($i % 2 == 0) ? 'even' : 'odd'; ?>
+                <tr>
+                <?php 
+                foreach ($audit_headers as $header): ?>
+                    <?php if (isset($audit_data_copy[$i]['Audit Notes']) && isset($audit_data_copy[$i]['asset_name'])) {
+                    if ($header === 'asset_tag') {
+                    echo "<td class='$color' style='color:green;font-weight:700;'>" . htmlspecialchars($audit_data_copy[$i][$header] ?? '') . "</td>";
+                    }  else {
+                    echo "<td class='$color'>" . htmlspecialchars($audit_data_copy[$i][$header] ?? '') . "</td>";
+                    }
+                } elseif ($header === 'asset_tag' && isset($audit_data_copy[$i]['asset_name'])) {
+                    echo "<td class='$color' style='color:red;font-weight:700;'>" . htmlspecialchars($audit_data_copy[$i][$header] ?? '') . "</td>";
+                } else if ($header === 'asset_tag' && (!isset($audit_data_copy[$i]['asset_name']) || !isset($audit_data_copy[$i]['Description']))) {
+                    echo "<td class='$color' style='color:orange;font-weight:700;'>" . htmlspecialchars($audit_data_copy[$i][$header] ?? '') . "</td>";
+                } else {
+                    echo "<td class='$color'>" . htmlspecialchars($audit_data_copy[$i][$header] ?? '') . "</td>";
+                }
+                    ?>
 
-</body>
+                 <?php endforeach; ?>
+
+                          
+                            </tr>
+                                            <?php endfor; ?>
+                    </tbody>
+                            </table>
+                                </div>
+                                        </section>
+                                            <?php } ?>
+                                            </body>
 </html>
 <?php 
             $html = ob_get_clean();
