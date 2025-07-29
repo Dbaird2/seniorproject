25eaa33ed2017dd62d29d5b9067bb668200d6586
Dason Baird <dbaird2@csub.edu>
2025-07-28 20:03:12 -0700

    Ajax.php

--- search/Ajax.php
+++ search/Ajax.php
@@ -435,8 +435,11 @@ if (isset($_POST['search']) || isset($_GET['search'])) {
         $json_bldg_id = json_encode($bldg_id);
         $json_dept_search= json_encode($dept_id_search);
         $json_price_check = json_encode($asset_price_check);
-?>
 
+?>
+        <script>
+        console.log(<?=$json_dept_search?>, <?=$json_price_check?>);
+        </script>
 <li class="page-item disabled">
       <a class="page-link" href="#" tabindex="-1">Previous</a>
     </li>
@@ -446,7 +449,8 @@ if (isset($_POST['search']) || isset($_GET['search'])) {
         <span class="sr-only">(current)</span>
       </span>
     </li>
- <li class="page-item"><a class="page-link" href="#" onclick="searchTriggerViaAjax(<?=$json_tag?>,<?=$json_cate?>,<?=$json_status?>,<?=$json_name?>,<?=$json_dept?>,<?=$json_dept_search?>,<?=$json_room_tag?>,<?=$json_room_loc?>,<?=$json_sn?>,<?=$json_bldg_name?>,<?=$json_price?>,<?=$json_bldg_id_val?>,<?=$json_po?>,<?=$json_bldg_id?>,<?=$json_price_check?>,<?=$offset+1?>)"><?=$offset+1?></a></li>
+
+ <li class="page-item"><a class="page-link" href="#" onclick='searchTriggerViaAjax(<?=$json_tag?>,<?=$json_cate?>,<?=$json_status?>,<?=$json_name?>,<?=$json_dept?>,<?=$json_dept_search?>,<?=$json_room_tag?>,<?=$json_room_loc?>,<?=$json_sn?>,<?=$json_bldg_name?>,<?=$json_price?>,<?=$json_bldg_id_val?>,<?=$json_po?>,<?=$json_bldg_id?>,<?=$json_price_check?>,<?=$offset+1?>)'><?=$offset+1?></a></li>
 <?php if ($total_pages > 2) { ?>
     <li class="page-item"><a class="page-link" href="#" onclick="searchTriggerViaAjax(<?=$json_tag?>,<?=$json_cate?>,<?=$json_status?>,<?=$json_name?>,<?=$json_dept?>,<?=$json_dept_search?>,<?=$json_room_tag?>,<?=$json_room_loc?>,<?=$json_sn?>,<?=$json_bldg_name?>,<?=$json_price?>,<?=$json_bldg_id_val?>,<?=$json_po?>,<?=$json_bldg_id?>,<?=$offset+2?>"><?=$offset+2?></a></li>
 <?php }
