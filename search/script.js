d214d82756dab30c8d0a8e9297fc9d2e1c0eed07
Dason Baird <dbaird2@csub.edu>
2025-07-28 19:48:40 -0700

    forgot ,

--- search/script.js
+++ search/script.js
@@ -117,7 +117,7 @@ function auditTrigger() {
     });
 }
 function searchTriggerViaAjax(
-      search, categories, statusFilter, box_name, dept_id, dept_id_search
+      search, categories, statusFilter, box_name, dept_id, dept_id_search,
       room_tag, room_loc, asset_sn, bldg_name,
       asset_price, bldg_id_search, asset_po, bldg_id, asset_price_check, offset
 ) {
