55ff7013e6c714ed2f63f4fb9f90ec670b43348c
Dason Baird <dbaird2@csub.edu>
2025-08-04 16:27:01 -0700

    complete-audit works

--- audit/audit-history/complete/complete-audit.php
+++ audit/audit-history/complete/complete-audit.php
@@ -12,4 +12,5 @@ if (isset($_POST)) {
         exit;
     }
 }
+exit;
 
--- audit/audit-history/continue/get-audit-hist-data.php
+++ audit/audit-history/continue/get-audit-hist-data.php
@@ -7,6 +7,17 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 if (isset($_POST)) {
     $dept_id = $_GET['dept_id'];
     $audit_id = (int)$_GET['audit_id'];
+    $audit_type_map [
+        1 => 'cust',
+        2 => 'cust',
+        3 => 'mgmt',
+        4 => 'mgmt',
+        5 => 'FDN',
+        6 => 'FDN',
+        7 => 'SPA',
+        8 => 'SPA'
+    ];
+    $audit_type = $audit_type_map[$audit_id];
     try {
         $select_q = "SELECT auditor, audit_data FROM audit_history WHERE dept_id = :dept_id AND audit_id = :audit_id";
         $select_stmt = $dbh->prepare($select_q);
