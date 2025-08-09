c0960c32535f62081752130aebbfc91673638f6f
Dason Baird <dbaird2@csub.edu>
2025-08-08 20:10:10 -0700

    stmt fix

--- kualiAPI/asset-received.php
+++ kualiAPI/asset-received.php
@@ -102,7 +102,7 @@ try {
             $name = $tag['data']['vNv8CdzZjv'];
             $select_q = "SELECT asset_tag FROM asset_info WHERE asset_tag = :tag";
             $s_stmt = $dbh->prepare($select_q);
-            $s_stmt = $s_stmt->execute([":tag" => $tag_num]);
+            $s_stmt->execute([":tag" => $tag_num]);
             $tag_taken = $s_stmt->fetch(PDO::FETCH_ASSOC);
             if (!$tag_taken) {
                 $insert_q = "INSERT INTO asset_info (asset_tag, asset_name, date_added, serial_num, asset_price, asset_model, po, dept_id, lifecycle) VALUES
