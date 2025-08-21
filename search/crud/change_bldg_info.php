18fbc341b1ec1eb3a05afbfc66ef066a55e77b9c
Dason Baird <dbaird2@csub.edu>
2025-08-21 13:51:51 -0700

    simplified the logic in change_bldg

--- search/crud/change_bldg_info.php
+++ search/crud/change_bldg_info.php
@@ -14,61 +14,31 @@ if (isset($_POST['bldg'])) {
     $new_tag =trim($_POST['room_tag']);
 
     try {
-        $set_array = [];
-        $bldg_params = [":old_name"=>$old_name];
-        $bldg_params[":old_id"]= $old_id;
-        $bldg_id_change = false;
-        if ($new_id !== '' && $new_id !== $old_id) {
-            $bldg_id_change = true;
-            $set_array[] = "bldg_id = :bldg_id";
-            $bldg_params[":bldg_id"] = $new_id;
+        if ($old_id !== $new_id && !empty($new_id)) {
+            $update_bldg = "UPDATE bldg_table SET bldg_id = :new_bldg WHERE bldg_id = :old_bldg";
+            $bldg_stmt = $dbh->prepare($update_bldg);
+            $bldg_stmt->execute([":new_bldg"=>$new_id, ":old_bldg"=>$old_id]);
+
+            $update_room = "UPDATE room_table SET bldg_id = :new_bldg WHERE bldg_id = :old_bldg";
+            $room_stmt = $dbh->prepare($update_room);
+            $room_stmt->execute([":new_bldg"=>$new_id, ":old_bldg"=>$old_id]);
         }
-        if ($new_name !== '' && $new_name !== $old_name) {
-            $set_array[] = "bldg_name = :bldg_name";
-            $bldg_params[":bldg_name"] = $new_name;
+        if ($old_name !== $new_name && !empty($new_name)) {
+            $update_bldg = "UPDATE bldg_table SET bldg_name = :new_bldg WHERE bldg_name = :old_bldg";
+            $bldg_stmt = $dbh->prepare($update_bldg);
+            $bldg_stmt->execute([":new_bldg"=>$new_name, ":old_bldg"=>$old_name]);
         }
-        if (!empty($set_array)) {
-            $set_bldg = implode(', ', $set_array);
-            $select_q = "SELECT bldg_id, bldg_name  FROM bldg_table WHERE bldg_id = :bldg_id AND bldg_name = :bldg_name";
-            $select_stmt = $dbh->prepare($select_q);
-            $select_stmt->execute([":bldg_id"=>$old_id, ":bldg_name"=>$new_name]);
-            if ($select_stmt->rowCount() > 0) {
-                $update_q = "UPDATE bldg_table SET " . $set_bldg . ' WHERE bldg_id = :old_id AND bldg_name = :old_name';
-                $update_stmt = $dbh->prepare($update_q);
-                $update_stmt->execute($params);
-            }
+        if ($old_loc !== $new_loc && !empty($new_loc)) {
+            $update_room = "UPDATE room_table SET room_loc = :new_loc WHERE room_loc = :old_loc";
+            $room_stmt = $dbh->prepare($update_room);
+            $room_stmt->execute([":new_loc"=>$new_loc, ":old_loc"=>$old_loc]);
         }
-    } catch (PDOException $e) {
-        error_log("Error " . $e->getMessage());
-        exit;
-    } 
-    try {
-        $set_array = [];
-        $room_params = [":old_tag"=>$old_tag];
-        if (!empty($new_loc) && $new_loc !== $old_loc) {
-            $set_array[] = "room_loc = :new_loc";
-            $room_params[":new_loc"] = $new_loc;
+        if ($old_tag !== $new_tag && !empty($new_tag)) {
+            $update_room = "UPDATE room_table SET room_tag = :new_tag WHERE room_tag = :old_tag";
+            $room_stmt = $dbh->prepare($update_room);
+            $room_stmt->execute([":new_tag"=>$new_tag, ":old_tag"=>$old_tag]);
         }
-        if (!empty($new_tag) && $new_tag !== $old_tag) {
-            $set_array[] = "room_tag = :new_tag";
-            $room_params[":new_tag"] = $new_tag;
-        }
-        if (!empty($set_array)) {
-            $set_room = implode(', ', $set_array);
-            $select_q = "SELECT room_loc, room_tag FROM room_table WHERE room_tag = :new_tag";
-            $select_stmt = $dbh->prepare($select_q);
-            $select_stmt->execute([":new_tag"=>$new_tag]);
-            if ($select_stmt->rowCount() > 0) {
-                $update_q = "UPDATE room_table SET " . $set_room . ' WHERE room_tag = :old_tag';
-                $update_stmt = $dbh->prepare($update_q);
-                $update_stmt->execute($room_params);
-            }
-        }
-        if ($bldg_id_change === true) {
-            $update_rooms = "UPDATE room_table set bldg_id = :bldg_id WHERE bldg_id = :old_bldg_id";
-            $update_stmt = $dbh->prepare($update_rooms);
-            $update_stmt->execute([':bldg_id'=>$new_id, ":old_bldg_id"=>$old_id]);
-        }
+
     } catch (PDOException $e) {
         error_log("Error " . $e->getMessage());
         exit;
