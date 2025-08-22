15190265679ed07e71e2d81ec79ebae2c9b21f4c
Dason Baird <dbaird2@csub.edu>
2025-08-22 12:44:04 -0700

    fixed joins not working in audititng.php

--- audit/auditing.php
+++ audit/auditing.php
@@ -134,7 +134,9 @@ if (isset($_POST['data']) && isset($_POST['dynamicInput']) && ($_POST['dynamicIn
                 }
                 if ($found === 0) {
                     $select_q = "SELECT a.asset_name, a.serial_num, r.room_loc, b.bldg_id, a.dept_id, a.asset_price, a.po, a.bus_unit, d.custodian, a.date_added
-                        FROM asset_info AS a JOIN room_table AS r ON r.room_tag = a.room_tag JOIN bldg_table AS b ON b.bldg_id = r.bldg_id JOIN department AS d ON a.dept_id = d.dept_id
+                        FROM asset_info AS a LEFT JOIN room_table AS r ON r.room_tag = a.room_tag 
+                        LEFT JOIN bldg_table AS b ON b.bldg_id = r.bldg_id 
+                        LEFT JOIN department AS d ON a.dept_id = d.dept_id
                         WHERE a.asset_tag = :tag";
                     $select_stmt = $dbh->prepare($select_q);
                     $select_stmt->execute([":tag" => $tag]);
