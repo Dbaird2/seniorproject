e02f2e48c66a3e52b68a784898012b2392703e53
Dason Baird <dbaird2@csub.edu>
2025-09-13 18:06:42 -0700

    forgot it session email

--- asset-manager/color.php
+++ asset-manager/color.php
@@ -1,20 +1,19 @@
 <?php
 require_once "../config.php";
 header('Content-Type: application/json');
+error_reporting(E_ALL);
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
 
 $json = file_get_contents('php://input');
 
 $post_data = json_decode($json, true);
 
-if (json_last_error() !== JSON_ERROR_NONE) {
-    echo "JSON decoding error: " . json_last_error_msg();
-    exit();
-}
 
 $update = "UPDATE user_asset_profile SET color = :color WHERE profile_name = :profile AND email = :email";
 try {
     $update_stmt = $dbh->prepare($update);
-    $update_stmt->execute([":color"=>$post_data['color'], ':profile'=>$post_data['profile_name'], ':email'=>$post_data['email']]);
+    $update_stmt->execute([":color"=>$post_data['color'], ':profile'=>$post_data['profile_name'], ':email'=>$_SESSION['email']]);
 } catch (PDOException $e) {
     error_log($e->getMessage());
     exit;
