a8763af9bd8b0996c9a17dc1067381de6a3b8de4
Dason Baird <dbaird2@csub.edu>
2025-10-03 20:00:18 -0700

    replaced with pass_verify

--- phone-api/check-login.php
+++ phone-api/check-login.php
@@ -1,5 +1,5 @@
 <?php 
-header("Access-Control-Allow-Oirigin: *");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 include_once "../config.php";
@@ -7,48 +7,27 @@ $decoded_data = file_get_contents('php://input');
 $data = json_decode($decoded_data, true);
 $pw = trim($data['pw']);
 $email = trim($data['email']);
+if (!empty($email) || !empty($pw)) {
+    echo json_encode(['status'=>'Failed to login']);
+    exit;
+}
 
-$hashed_pw = password_hash($pw, PASSWORD_DEFAULT);
 
 try {
-    $select = "SELECT phone_key FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
+    $select = "SELECT phone_key, pw FROM user_table WHERE (email = :email OR username = :email) limit 1";
     $stmt = $dbh->prepare($select);
-    $stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
+    $stmt->execute([":email"=>$email]);
 } catch (PDOException $e) {
     $msg = $e->getMessage();
     echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
     exit;
 }
 if ($stmt->rowCount() > 0) {
-    $length = 32; // Number of bytes for the random string, results in 64 hex characters
-    $api_key = bin2hex(random_bytes($length));
-
-    try {
-        $update = "UPDATE user_table SET phone_key = :api WHERE email = :email OR username = :email";
-        $stmt = $dbh->prepare($update);
-        $stmt->execute([':api'=>$api_key, ":email"=>$email]);
-    } catch (PDOException $e) {
-        $msg = $e->getMessage();
-        echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
-        exit;
-    }
-
-
-    echo json_encode(['status'=>'Successfully logged in','api_key'=>$api_key]);
-    exit;
-} else {
-    try {
-        $select_user = "SELECT username FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
-        $stmt = $dbh->prepare($select);
-        $stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
-    } catch (PDOException $e) {
-        $msg = $e->getMessage();
-        echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
-        exit;
-    }
-    if ($stmt->rowCount() > 0) {
+    $info = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (password_verify($pw, $info['pw']) {
         $length = 32; // Number of bytes for the random string, results in 64 hex characters
         $api_key = bin2hex(random_bytes($length));
+
         try {
             $update = "UPDATE user_table SET phone_key = :api WHERE email = :email OR username = :email";
             $stmt = $dbh->prepare($update);
@@ -61,6 +40,35 @@ if ($stmt->rowCount() > 0) {
         echo json_encode(['status'=>'Successfully logged in','api_key'=>$api_key]);
         exit;
     }
+
+} else {
+    try {
+        $select_user = "SELECT username FROM user_table WHERE (email = :email OR username = :email) limit 1";
+        $stmt = $dbh->prepare($select);
+        $stmt->execute([":email"=>$email]);
+    } catch (PDOException $e) {
+        $msg = $e->getMessage();
+        echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
+        exit;
+    }
+    if ($stmt->rowCount() > 0) {
+        $info = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($pw, $info['pw']) {
+            $length = 32; // Number of bytes for the random string, results in 64 hex characters
+            $api_key = bin2hex(random_bytes($length));
+            try {
+                $update = "UPDATE user_table SET phone_key = :api WHERE email = :email OR username = :email";
+                $stmt = $dbh->prepare($update);
+                $stmt->execute([':api'=>$api_key, ":email"=>$email]);
+            } catch (PDOException $e) {
+                $msg = $e->getMessage();
+                echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
+                exit;
+            }
+            echo json_encode(['status'=>'Successfully logged in','api_key'=>$api_key]);
+            exit;
+        }
+    }
 }
-echo json_encode(['status'=>'Failed to login', 'email/user'=>$email, 'pw'=>$pw, 'hashed'=>$hashed_pw]);
+echo json_encode(['status'=>'Failed to login']);
 exit;
