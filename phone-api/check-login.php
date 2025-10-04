199b6b4bd34826363eecf76d31b92f70250186f6
Dason Baird <dbaird2@csub.edu>
2025-10-03 19:14:21 -0700

    fixed typo, changed to post

--- phone-api/check-login.php
+++ phone-api/check-login.php
@@ -3,16 +3,18 @@ header("Access-Control-Allow-Oirigin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 include_once "../config.php";
-$decoded_data = file_get_contents('php://input');
-$data = json_decode($decoded_data, true);
-$pw = trim($data['pw']);
-$email = trim($data['email']);
+if (!isset($_POST['pw'])) {
+    echo json_encode(['status'=>'Login failed']);
+    exit;
+}
+$pw = trim($_POST['pw']);
+$email = trim($_POST['email']);
 
 $hashed_pw = password_hash($pw, PASSWORD_DEFAULT);
 
 $select = "SELECT phone_key FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
 $stmt = $dbh->prepare($select);
-$stmt->execute([":email"=>$email, ":pw"=>$hash_pw]);
+$stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
 if ($stmt->rowCount() > 0) {
     $length = 32; // Number of bytes for the random string, results in 64 hex characters
     $api_key = bin2hex(random_bytes($length));
@@ -25,7 +27,7 @@ if ($stmt->rowCount() > 0) {
 } else {
     $select_user = "SELECT username FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
     $stmt = $dbh->prepare($select);
-    $stmt->execute([":email"=>$email, ":pw"=>$hash_pw]);
+    $stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
     if ($stmt->rowCount() > 0) {
         $length = 32; // Number of bytes for the random string, results in 64 hex characters
         $api_key = bin2hex(random_bytes($length));
