7b35fb6281a73afd2902f0b7cc62615808b33cf9
Dason Baird <dbaird2@csub.edu>
2025-10-03 19:41:53 -0700

    adding catches

--- phone-api/check-login.php
+++ phone-api/check-login.php
@@ -10,29 +10,54 @@ $email = trim($data['email']);
 
 $hashed_pw = password_hash($pw, PASSWORD_DEFAULT);
 
-$select = "SELECT phone_key FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
-$stmt = $dbh->prepare($select);
-$stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
+try {
+    $select = "SELECT phone_key FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
+    $stmt = $dbh->prepare($select);
+    $stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
+} catch (PDOException $e) {
+    $msg = $e->getMessage();
+    echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
+    exit;
+}
 if ($stmt->rowCount() > 0) {
     $length = 32; // Number of bytes for the random string, results in 64 hex characters
     $api_key = bin2hex(random_bytes($length));
 
-    $update = "UPDATE user_table SET phone_key = :api WHERE email = :email OR username = :email";
-    $stmt = $dbh->prepare($update);
-    $stmt->execute([':api'=>$api_key, ":email"=>$email]);
-    echo json_encode(['status'=>'Successfully logged in','api_key'=>$api_key]);
-    exit;
-} else {
-    $select_user = "SELECT username FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
-    $stmt = $dbh->prepare($select);
-    $stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
-    if ($stmt->rowCount() > 0) {
-        $length = 32; // Number of bytes for the random string, results in 64 hex characters
-        $api_key = bin2hex(random_bytes($length));
-
+    try {
         $update = "UPDATE user_table SET phone_key = :api WHERE email = :email OR username = :email";
         $stmt = $dbh->prepare($update);
         $stmt->execute([':api'=>$api_key, ":email"=>$email]);
+    } catch (PDOException $e) {
+        $msg = $e->getMessage();
+        echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
+        exit;
+    }
+
+
+    echo json_encode(['status'=>'Successfully logged in','api_key'=>$api_key]);
+    exit;
+} else {
+    try {
+        $select_user = "SELECT username FROM user_table WHERE (email = :email OR username = :email) AND pw = :pw";
+        $stmt = $dbh->prepare($select);
+        $stmt->execute([":email"=>$email, ":pw"=>$hashed_pw]);
+    } catch (PDOException $e) {
+        $msg = $e->getMessage();
+        echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
+        exit;
+    }
+    if ($stmt->rowCount() > 0) {
+        $length = 32; // Number of bytes for the random string, results in 64 hex characters
+        $api_key = bin2hex(random_bytes($length));
+        try {
+            $update = "UPDATE user_table SET phone_key = :api WHERE email = :email OR username = :email";
+            $stmt = $dbh->prepare($update);
+            $stmt->execute([':api'=>$api_key, ":email"=>$email]);
+        } catch (PDOException $e) {
+            $msg = $e->getMessage();
+            echo json_encode(['status'=>'Error with database', 'error'=>$msg]);
+            exit;
+        }
         echo json_encode(['status'=>'Successfully logged in','api_key'=>$api_key]);
         exit;
     }
