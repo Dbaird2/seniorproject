c18d343018562d112e94d084b0b8f7d3c79c8d69
Dason Baird <dbaird2@csub.edu>
2025-10-09 15:23:49 -0700

    should be syntax issue free

--- kualiAPI/transfer.php
+++ kualiAPI/transfer.php
@@ -185,7 +185,7 @@ function bldgChange() {
             $room_loc = $data['data']['6JHs3W0-CL'];
         }
         $bldg_name = ['data']['hXHmCy0mek']['label'];
-        if (!empty($bldg_name) && !empty($room_loc) {
+        if (!empty($bldg_name) && !empty($room_loc)) {
             checkBldg($bldg_name, $room_loc);
         }
 
