53bc0bbced19aef477e487be728719c71be79aad
Dason Baird <dbaird2@csub.edu>
2025-10-14 20:53:36 -0700

    fixing queries in phone-api

--- phone-api/get-all-assets.php
+++ phone-api/get-all-assets.php
@@ -10,13 +10,13 @@ $decoded_data = file_get_contents('php://input');
 $data = json_decode($decoded_data);
 
 if (isset($_POST)) {
-    $select_count = "SELECT COUNT(*) FROM asset_info";
+    $select_count = "SELECT COUNT(*) FROM asset_info WHERE asset_status = 'In Service'";
     $select_stmt = $dbh->query($select_count);
     $row_count = $select_stmt->fetchColumn();
     $select = "SELECT a.asset_tag, a.serial_num, a.po, a.bus_unit,
             a.asset_name, a.asset_price, a.room_tag, a.dept_id, b.bldg_name, r.room_loc FROM asset_info AS a
-            JOIN room_table AS r ON a.room_tag = r.room_tag
-            JOIN bldg_table AS b ON r.bldg_id = b.bldg_id";
+            LEFT JOIN room_table AS r ON a.room_tag = r.room_tag
+            LEFT JOIN bldg_table AS b ON r.bldg_id = b.bldg_id WHERE asset_status = 'In Service' ORDER BY a.asset_tag";
     $select_stmt = $dbh->query($select);
     $data = $select_stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode(["data" =>$data, 'count'=>$row_count]);
--- phone-api/get-all-users.php
+++ phone-api/get-all-users.php
@@ -10,7 +10,7 @@ $decoded_data = file_get_contents('php://input');
 $data = json_decode($decoded_data);
 
 if (isset($_POST)) {
-    $select_count = "SELECT COUNT(*) FROM user_table WHERE u_role = 'custodian'";
+    $select_count = "SELECT COUNT(*) FROM user_table";
     $select_stmt = $dbh->query($select_count);
     $row_count = $select_stmt->fetchColumn();
     $select = "SELECT CONCAT(f_name ,' ', l_name) as custodian, STRING_AGG(c, ',') as dept_id FROM user_table, unnest(dept_id) as c group by custodian;";
