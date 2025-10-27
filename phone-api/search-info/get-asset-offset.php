32337345cd880e641bfcb31ab40198cf741baa6d
Dason Baird <dbaird2@csub.edu>
2025-10-26 18:56:01 -0700

    fixing decode & names

--- phone-api/search-info/get-asset-offset.php
+++ phone-api/search-info/get-asset-offset.php
@@ -7,15 +7,20 @@ include_once "../config.php";
 
 $decoded_data = file_get_contents('php://input');
 
-$data = json_decode($decoded_data);
+$data = json_decode($decoded_data, true);
 $offset = $data['offset'];
 $limit = $data['limit'];
 $search = $data['search'];
+$echo = function($array) {
+    echo '<pre>';
+    var_dump($array);
+    echo '</pre>';
+}
 
 if (isset($_POST)) {
     $dept_regex = "/^D\d+/i";
-    $select = "SELECT a.asset_tag, a.serial_num, a.po, a.bus_unit,
-            a.asset_name, a.asset_price, a.room_tag, a.dept_id, b.bldg_name, r.room_loc FROM asset_info AS a
+    $select = "SELECT a.asset_tag as tag, a.serial_num as serial, a.po, a.bus_unit,
+            a.asset_name as name, a.asset_price as price, a.room_tag, a.dept_id, b.bldg_name, r.room_loc FROM asset_info AS a
             LEFT JOIN room_table AS r ON a.room_tag = r.room_tag
             LEFT JOIN bldg_table AS b ON r.bldg_id = b.bldg_id WHERE asset_status = 'In Service' ";
     $params = [];
