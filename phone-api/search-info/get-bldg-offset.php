d42cc66fb4a15ac924319276b8cc7b7b0363d343
Dason Baird <dbaird2@csub.edu>
2025-10-26 22:53:31 -0700

    casting bldgid and room_tag

--- phone-api/search-info/get-bldg-offset.php
+++ phone-api/search-info/get-bldg-offset.php
@@ -16,7 +16,7 @@ if (isset($_POST)) {
     $select = "SELECT b.bldg_id, b.bldg_name, r.room_tag, r.room_loc FROM bldg_table b left join room_table r on r.bldg_id = b.bldg_id WHERE 1=1 ";
     $params = [];
     if (!empty($search)) {
-        $select .= " AND (b.bldg_name = :bldg OR b.bldg_id = :bldg OR r.room_tag = :bldg) ";
+        $select .= " AND (b.bldg_name = :bldg OR CAST(b.bldg_id AS TEXT) = :bldg OR CAST(r.room_tag AS TEXT) = :bldg) ";
         $params[':bldg'] = $search;
     }
     $select .= " ORDER BY bldg_name LIMIT :limit OFFSET :offset";
