e63f8be2f6ad22b8ff06d1c67bea353f246357a1
Dason Baird <dbaird2@csub.edu>
2025-11-09 19:59:14 -0800

    forgot to change path for config

--- phone-api/audit/start-audit.php
+++ phone-api/audit/start-audit.php
@@ -2,12 +2,13 @@
 header("Access-Control-Allow-Oirigin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
-include_once "../config.php";
+include_once "../../config.php";
 
 
 $decoded_data = file_get_contents('php://input');
 
 $data = json_decode($decoded_data);
+
 
 if (isset($_POST['dept_name'])) {
     $select = "SELECT a.asset_tag, a.asset_name, a.serial_num, d.dept_name, a.po, CONCAT(b.bldg_name, ' ', r.room_loc) as Location FROM asset_info a LEFT JOIN department d ON a.dept_id = d.dept_id LEFT JOIN room_table r ON a.room_tag = r.room_tag LEFT JOIN bldg_table b on r.bldg_id = b.bldg_id WHERE dept_name = :name";
