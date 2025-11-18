f1b90619a110a5e20ecb23cfb0de754f92d9c928
Dason Baird <dbaird2@csub.edu>
2025-11-17 22:32:03 -0800

    moving ++

--- kualiAPI/write/complete-audit.php
+++ kualiAPI/write/complete-audit.php
@@ -78,12 +78,14 @@ if ($audit_info) {
                     echo 'Transfer <br>';
                     $variables['data']['HgIvQwEnwb']['data'][$count]['data']['xVdCwxjKl-'][] = trim($tag);
                     $variables['data']['HgIvQwEnwb']['data'][$count]['data']['2KqtRaCah1'][] = 'Transfer update after auditing';
-                    $variables['data']['HgIvQwEnwb']['data'][$count]['id'][] =(string) $count++;
+                    $variables['data']['HgIvQwEnwb']['data'][$count]['id'][] =(string) $count;
+                    $count++;
                     $transfer_status = true;
                 } else if ($lsd) {
                     echo 'Lsd <br>';
                     $variables['data']['g3eXi7dYR2']['data'][$count2]['data']['vJyySSnsqZ'][] = trim($tag);
-                    $variables['data']['HgIvQwEnwb']['data'][$count2]['id'][] = (string) $count2++;
+                    $variables['data']['HgIvQwEnwb']['data'][$count2]['id'][] = (string) $count2;
+                    $count2++;
                     $lsd_status = true;
                 }
             }
