08bbd00edf47e9817f42872071128560b72cae55
Dason Baird <dbaird2@csub.edu>
2025-11-21 15:58:47 -0800

    removed old manager

--- kualiAPI/write/transfer.php
+++ kualiAPI/write/transfer.php
@@ -127,25 +127,6 @@ if ($data['form_type'] === 'dept') {
             $variables['data']['xPQtXjuWnk']['label'] = 'No';
         }
 
-        // GET CURRENT MANAGER INFO
-        $stmt = $dbh->prepare($dept_select);
-        $stmt->execute([':id'=>$_SESSION['deptid']]);
-        $current_manager = $stmt->fetchColumn();
-/*
-$email_regex = '/\b(@)\b/i';
-if (preg_match($email_regex, $
- */
-        $current_manager_info = getNameInfo($current_manager, $audit_dept);
-        $variables['data']['u7YkM8hmb-']['displayName'] = $current_manager_info['displayName'];
-        $variables['data']['u7YkM8hmb-']['email'] = $current_manager_info['email'];
-        $variables['data']['u7YkM8hmb-']['firstName'] = $current_manager_info['firstName'];
-        $variables['data']['u7YkM8hmb-']['id'] = $current_manager_info['id'];
-        $variables['data']['u7YkM8hmb-']['label'] = $current_manager_info['label'];
-        $variables['data']['u7YkM8hmb-']['lastName'] = $current_manager_info['lastName'];
-        $variables['data']['u7YkM8hmb-']['schoolId'] = $current_manager_info['schoolId'];
-        $variables['data']['u7YkM8hmb-']['username'] = $current_manager_info['username'];
-
-
         // MANAGER
         $manager = trim($dept_info['dept_manager']);
         $manager_info = getNameInfo($manager, $dept_info['dept_id']);
