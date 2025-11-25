2b7be623e137ccd8fda14ab86a0da8b4f7ac5045
Dason Baird <dbaird2@csub.edu>
2025-11-25 10:15:57 -0800

    adding assets that have no building location

--- insert_assets.php
+++ insert_assets.php
@@ -80,7 +80,12 @@ if (isset($_FILES['file'])) {
 
     if (count($location_array) === 2) {
         if ((int)$location_array[0] === 0) {
-            echo 'Invalid Building ID';
+            echo 'Invalid Building ID<br>';
+            $insert = 'INSERT INTO asset_info (asset_tag, asset_name, date_added, serial_num, asset_price, asset_status, asset_type, dept_id, is_it, fund) VALUES
+                (?,?,?,?,?,?,?,?,?,?,?) ON CONFLICT (asset_tag) DO UPDATE SET fund = EXCLUDED.fund, is_it = EXCLUDED.is_it';
+            $stmt = $dbh->prepare($insert);
+            $stmt->execute([$row[3], $row[4], $row[13], $row[6], 1.00, $status, $row[14], $row[9], $is_it, $row[10]]);
+            echo 'Insert/Updated No Location<br>';
             continue;
         }
         if ($location_array[0] === '44A') {
@@ -103,7 +108,15 @@ if (isset($_FILES['file'])) {
       $stmt = $dbh->prepare($insert);
       $stmt->execute([$row[3], $row[4], $row[13], $row[6], 1.00, $status, $row[14], $room_tag, $row[9], $is_it, $row[10]]);
       echo 'Inserted tag<br>';
+    } else {
+        echo 'Invalid Building ID<br>';
+        $insert = 'INSERT INTO asset_info (asset_tag, asset_name, date_added, serial_num, asset_price, asset_status, asset_type, dept_id, is_it, fund) VALUES
+            (?,?,?,?,?,?,?,?,?,?,?) ON CONFLICT (asset_tag) DO UPDATE SET fund = EXCLUDED.fund, is_it = EXCLUDED.is_it';
+        $stmt = $dbh->prepare($insert);
+        $stmt->execute([$row[3], $row[4], $row[13], $row[6], 1.00, $status, $row[14], $row[9], $is_it, $row[10]]);
+        echo 'Insert/Updated No Location<br>';
     }
+
   }
 }
 
