9b20927e20ca8808233ac5b286a925b00577cca2
Dason Baird <dbaird2@csub.edu>
2025-11-26 20:42:19 -0800

    changing logic in update

--- audit/kuali/update-audit.php
+++ audit/kuali/update-audit.php
@@ -5,18 +5,15 @@ foreach ($_SESSION['data'] as $index => $asset) {
         continue;
     }
     $audit_id = $_SESSION['info'][5];
-    $select = 'SELECT asset_notes FROM asset_info WHERE asset_tag = :tag';
+    $select = "SELECT asset_notes FROM asset_info WHERE asset_tag = :tag AND asset_notes ~ '[CHCKD]'";
     $stmt = $dbh->prepare($select);
     $stmt->execute([':tag'=>$asset['Tag Number']]);
     $notes = $stmt->fetchColumn();
-    if (!empty($notes)) {
-        $regex = '/^CHCKD/';
-        if (preg_match($regex, $notes)) {
-            $_SESSION['data'][$index]['Found Notes'] .= $notes;
-            $_SESSION['data'][$index]['Tag Status'] = 'Found';
-        }
-    }
-    if ($_SESSION['data'][$index]['Tag Status'] !== 'Found') {
+    if ($notes) {
+        $_SESSION['data'][$index]['Found Notes'] .= $notes;
+        $_SESSION['data'][$index]['Tag Status'] = 'Found';
+        $_SESSION['data'][$index]['Found Room Number'] = 'CHCKD';
+    } else {
         $select = 'SELECT asset_tag, note, dept_id FROM audited_asset WHERE asset_tag = :tag AND audit_id = :id';
         $stmt = $dbh->prepare($select);
         $stmt->execute([':tag'=>$asset['Tag Number'], ':id'=>$audit_id]);
@@ -26,6 +23,7 @@ foreach ($_SESSION['data'] as $index => $asset) {
             $_SESSION['data'][$index]['Tag Status'] = 'Found';
         }
     }
+    $notes = $result = '';
 }
 echo json_encode(['status'=>'Ok']);
 exit;
