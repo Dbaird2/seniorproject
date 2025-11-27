fd2369b7c50dd5833a462ccf342e472c2013adc3
Dason Baird <dbaird2@csub.edu>
2025-11-26 16:56:44 -0800

    forgot the ['data']

--- audit/kuali/update-audit.php
+++ audit/kuali/update-audit.php
@@ -4,7 +4,7 @@ foreach ($_SESSION['data'] as $index => $asset) {
     if (in_array($asset['Tag Status'], ['Found', 'Extra'])) {
         continue;
     }
-    $audit_id = $SESSION['info'][5];
+    $audit_id = $_SESSION['info'][5];
     $select = 'SELECT asset_notes FROM asset_info WHERE asset_tag = :tag';
     $stmt = $dbh->prepare($select);
     $stmt->execute([':tag'=>$asset['Tag Number']]);
@@ -12,18 +12,18 @@ foreach ($_SESSION['data'] as $index => $asset) {
     if (!empty($notes)) {
         $regex = '/^CHCKD/';
         if (preg_match($regex, $notes)) {
-            $_SESSION[$index]['Found Notes'] .= $notes;
-            $_SESSION[$index]['Tag Status'] = 'Found';
+            $_SESSION['data'][$index]['Found Notes'] .= $notes;
+            $_SESSION['data'][$index]['Tag Status'] = 'Found';
         }
     }
-    if ($_SESSION[$index]['Tag Status'] !== 'Found') {
+    if ($_SESSION['data'][$index]['Tag Status'] !== 'Found') {
         $select = 'SELECT asset_tag, note, dept_id FROM audited_asset WHERE asset_tag = :tag AND audit_id = :id';
         $stmt = $dbh->prepare($select);
         $stmt->execute([':tag'=>$asset['Tag Number'], ':id'=>$audit_id]);
         $result = $stmt->fetch();
         if ($result) {
-            $_SESSION[$index]['Found Notes'] .= ' Found at ' . $result['dept_id'] . ' ';
-            $_SESSION[$index]['Tag Status'] = 'Found';
+            $_SESSION['data'][$index]['Found Notes'] .= ' Found at ' . $result['dept_id'] . ' ';
+            $_SESSION['data'][$index]['Tag Status'] = 'Found';
         }
     }
 }
