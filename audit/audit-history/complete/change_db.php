bba6cfae26604bb73a622a854875ef0a9dd7ac16
Dason Baird <dbaird2@csub.edu>
2025-12-04 11:09:20 -0800

    put exit for all reasons in change_db for it to not work in toast

--- audit/audit-history/complete/change_db.php
+++ audit/audit-history/complete/change_db.php
@@ -6,22 +6,29 @@ $data = json_decode($encoded_data, true);
 
 if (!empty($data)) {
     try {
-        $select = 'SELECT dept_id, dept_id FROM department WHERE dept_id = :dept';
+        $select = 'SELECT dept_id, dept_name, custodian[1] as cust, dept_manager FROM department WHERE dept_id = :dept';
         $stmt = $dbh->prepare($select);
         $stmt->execute([':dept'=$data['dept_id']]);
         $dept_data = $stmt->fetch();
-        if ($dept_data) {
-            $update = "UPDATE audit_history SET forms_submitted = true WHERE audit_id = :id AND dept_id = :dept";
-            $update_stmt = $dbh->prepare($update);
-            $update_stmt->execute([":id"=>$data['audit_id'], ":dept"=>$data['dept_id']]);
-            echo json_encode(['status'=>'Ok']);
-            exit;
-        } else {
-            echo json_encode(['status'=>'failed', 'reason'=>'department_not_exist']);
+        if (!isset($dept_data['dept_id'])) {
+            echo json_encode(['status'=>'failed', 'reason'=>'Department does not exist']);
             exit;
         }
+        if (empty($dept_data['dept_manager'])) {
+            echo json_encode(['status'=>'failed', 'reason'=>'Manager not set']);
+            exit;
+        }
+        if (empty($dept_data['cust'])) {
+            echo json_encode(['status'=>'failed', 'reason'=>'Custodian not set']);
+            exit;
+        }
+        $update = "UPDATE audit_history SET forms_submitted = true WHERE audit_id = :id AND dept_id = :dept";
+        $update_stmt = $dbh->prepare($update);
+        $update_stmt->execute([":id"=>$data['audit_id'], ":dept"=>$data['dept_id']]);
+        echo json_encode(['status'=>'Ok']);
+        exit;
     } catch (PDOException $e) {
-        echo json_encode(['status'=>'fail to update database']);
+        echo json_encode(['status'=>'failed', 'reason'=>'database failed']);
         exit;
     }
 } else {
